ed3f64e6cd28e29e1f0f1b6dc419a07486d770f2
Ivan Tur <ivantur2@gmail.com>
2021-02-26 23:38:32 +0200

    feat: update Menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,28 @@
+import { makeStyles } from "@material-ui/core";
+import NavButton from "./NavButton";
+
+const useStyles = makeStyles({
+  list: {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: '1rem'
+  },
+});
+
 const Menu: React.FC = () => {
-  return <div>Menu</div>;
+  const classes = useStyles();
+
+  return <div className={classes.list}>
+    <h1>Tic Tac Toe</h1>
+    <NavButton to='/game' text='Game' />
+    <NavButton to='/settings' text='Settings' />
+    <NavButton to='/statistics' text='Statistics' />
+    <NavButton to='/info' text='Info' />
+  </div>;
 }
 
 export default Menu;
