435d59ac0032e93bac7a650e88e9566008a0d30f
Ivan Tur <ivantur2@gmail.com>
2021-02-26 23:37:16 +0200

    feat: add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import Counter from "./components/Counter";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import Board from "./components/Board";
+import Info from "./components/Info";
+import Menu from "./components/Menu";
+import Settings from "./components/Settings";
+import Statistics from "./components/Statistics";
 
 function App() {
   return (
-    <div className="App">
-      <Counter />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={Menu} />
+          <Route path="/game" component={Board} />
+          <Route path="/info" component={Info} />
+          <Route path="/settings" component={Settings} />
+          <Route path="/statistics" component={Statistics} />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
