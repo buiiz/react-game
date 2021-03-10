b26e3d2bb7fc4f1b34f50b1db48df84b405bb846
Ivan Tur <ivantur2@gmail.com>
2021-03-10 13:24:20 +0200

    fix: `subst` => `substitute`

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -2,8 +2,8 @@ export const set = (name: string, value: any) => {
   window.localStorage.setItem(name, JSON.stringify(value));
 }
 
-export const get = (name: string, subst: any = null) => {
-  return JSON.parse(window.localStorage.getItem(name) || subst);
+export const get = (name: string, substitute: any = null) => {
+  return JSON.parse(window.localStorage.getItem(name) || substitute);
 }
 
 export const del = (name: string) => {
