02dfdbcdb49af393f2d7e3765087c16a42978a9c
Ivan Tur <ivantur2@gmail.com>
2021-03-11 16:26:44 +0200

    fix: remove thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,6 @@
-import { applyMiddleware, createStore } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from "redux-thunk";
+import { createStore } from "redux";
 import { rootReducer } from "./reducers";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer);
 
 export default store;
